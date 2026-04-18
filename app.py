d173c76074a067e056e5c05325f63e3e2829f402
ThuRein-110 <6731501110@lamduan.mfu.ac.th>
2026-04-18 21:22:28 +0700

    fixed

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def allowed_file(filename: str) -> bool:
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", "10000"))
+    app.run(host="0.0.0.0", port=port)
